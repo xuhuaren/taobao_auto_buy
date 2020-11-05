37aedb8b39355a440fdb7c3fcf885d4e863bda05
xuhuaren <xuhuaren@tencent.com>
2020-11-05 20:02:40 +0800

    update

--- lib/base.py
+++ lib/base.py
@@ -52,7 +52,7 @@ class AutoBuyBase(object):
     def _validate_time(self, time):
 
         try:
-            datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
+            datetime.strptime(time, "%Y-%m-%d %H:%M:%S:%f")
         except:
             raise InvalidInputTime()
 
@@ -87,7 +87,7 @@ class AutoBuyBase(object):
 
         opts = ChromeOptions()
         #opts.add_experimental_option("detach", True)
-        #opts.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2}) # 不加载图片,加快访问速度
+        opts.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2}) # 不加载图片,加快访问速度
         opts.add_experimental_option('excludeSwitches', ['enable-automation']) # 此步骤很重要，设置为开发者模式，防止被各大网站识别出来使用了Selenium
         driver_dir = self._get_driver_dir()
         self._logger.info(f"using {driver_dir}")
@@ -169,7 +169,8 @@ class AutoBuyBase(object):
 
     def _timer(self, buy_time):
 
-        buy_time_raw = datetime.strptime(buy_time, "%Y-%m-%d %H:%M:%S")
+        buy_time_raw = datetime.strptime(buy_time, "%Y-%m-%d %H:%M:%S:%f")
+        print(buy_time_raw)
         while datetime.now() < buy_time_raw:
             self._timer_printer(buy_time_raw)
 
